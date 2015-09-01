6cfbf8007e628d5f0895de2858dfd715b429c628
Rockford Lhotka <rocky@lhotka.net>
2015-09-01 16:52:26 -0500

    Closes #3 Add support for .NET 4.6

--- Bxf.Wpf.4.6/Properties/AssemblyInfo.cs
+++ Bxf.Wpf.4.6/Properties/AssemblyInfo.cs
@@ -1,16 +1,22 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.lhotka.net/1.0.0/bxfc", "Bxf.Converters")]
+[assembly: XmlnsPrefix("http://schemas.lhotka.net/1.0.0/bxfc", "bxfc")]
+[assembly: XmlnsDefinition("http://schemas.lhotka.net/1.0.0/bxf", "Bxf.Xaml")]
+[assembly: XmlnsPrefix("http://schemas.lhotka.net/1.0.0/bxf", "bxf")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bxf.Wp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Basic XAML Framework")]
+[assembly: AssemblyDescription("Bxf for WPF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Bxf.Wp")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("Marimer LLC")]
+[assembly: AssemblyProduct("Bxf")]
+[assembly: AssemblyCopyright("Copyright © Marimer LLC 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bde252c5-2a07-4be9-878e-9f2a33e377be")]
+[assembly: Guid("6b420836-e229-4471-9160-44ccb891ca16")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +35,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
--- Bxf.Wpf/Properties/AssemblyInfo.cs
+++ Bxf.Wpf/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Windows.Markup;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marimer LLC")]
 [assembly: AssemblyProduct("Bxf")]
-[assembly: AssemblyCopyright("Copyright © Marimer LLC 2010")]
+[assembly: AssemblyCopyright("Copyright © Marimer LLC 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -38,5 +38,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
